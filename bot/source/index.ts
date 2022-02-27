c21864dd78e0796cc6a6dbf2c84ceb4a9546f1b7
fiigmnt <fiigmnt@protonmail.com>
2022-02-27 15:34:58 -0500

    moved api to env

--- bot/source/index.ts
+++ bot/source/index.ts
@@ -4,9 +4,7 @@ import axios from "axios";
 import * as dot from "dotenv";
 dot.config();
 
-const { DISCORD_TOKEN } = process.env;
-
-const API = "http://localhost:8080/api";
+const { API, DISCORD_TOKEN } = process.env;
 
 const client = new Client({
   intents: [
@@ -25,6 +23,7 @@ interface Response {
   channelId: string;
   emoji: string;
   reactionCount: number;
+  rewardAmount: number;
 }
 
 client.on("messageReactionAdd", async (ReactionEmoji) => {
@@ -34,8 +33,6 @@ client.on("messageReactionAdd", async (ReactionEmoji) => {
     if (reaction.partial) {
       await reaction.fetch();
     }
-
-    console.log(reaction);
 
     const response: Response = (
       await axios.post(`${API}/discord`, {
@@ -55,14 +52,26 @@ client.on("messageReactionAdd", async (ReactionEmoji) => {
       };
 
       if (sendMessage()) {
-        const channel = client.channels.cache.get(response.channelId);
+        const channel = client.channels.cache.get(
+          response.channelId
+        ) as TextChannel;
         const formattedMessage = `Shared by @${message?.author?.username}\n${message.content}`;
-        if (channel as TextChannel) {
-          (channel as TextChannel).send(formattedMessage);
+        if (channel) {
+          channel.send(formattedMessage);
           console.log("Message sent.");
           console.log(formattedMessage);
         } else {
           console.log("Issue with curation channel");
+        }
+
+        if (response.rewardAmount > 0 && reaction.message.author?.username) {
+          // send reward to user
+          const { username } = reaction.message.author;
+          const { rewardAmount } = response;
+          const rewardMessage = `!coin send @${username} ${rewardAmount}`;
+          const rewardDescription = `Rewarding @${username} ${rewardAmount} for their curated post in #${channel.name}`;
+          reaction.message.channel.send(rewardDescription);
+          reaction.message.channel.send(rewardMessage);
         }
       }
     }
