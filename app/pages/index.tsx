42f1e674572a1966ca1d4fc0343a8ba8e7f36010
fiigmnt <fiigmnt@protonmail.com>
2022-02-27 19:31:45 -0500

    Added landing page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,28 +3,23 @@ import Head from "next/head";
 import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Header from "../components/Header";
+import Landing from "../components/Landing";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
-
-  return (
-    <>
-      <Header />
-    </>
-  );
   if (session) {
-    console.log(session);
     return (
       <>
-        Signed in as {session?.user?.email} <br />
+        <Header />
+        DASHBOARD
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn("discord")}>Sign in</button>
+      {/* <Header /> */}
+      <Landing />
     </>
   );
 };
